275079919fe9200c271eb014716fea081b18c023
Admins <Admins@DESKTOP-K9H1G5B>
2018-03-20 23:21:04 +0700

    init
    Change-Id: I4769ba415d403b54b9c8f780c61cbcb1fa687e4f

---
+++ src/main/java/com/example/springwebfluxanotations/SpringWebfluxAnotationsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springwebfluxanotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxAnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxAnotationsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/springwebfluxanotations/Student.java
@@ -0,0 +1,11 @@
+package com.example.springwebfluxanotations;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Student {
+
+	private String name;
+}
